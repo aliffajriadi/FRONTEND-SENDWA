2c04904e3197a2300a34ada1d959795124605c87
aliffajriadi <aliffajriadi@gmail.com>
2025-05-10 00:17:58 +0700

    ganti status maintain

--- src/components/Status.vue
+++ src/components/Status.vue
@@ -16,11 +16,22 @@ onMounted(()=>{
 </script>
 
 <template>
-  <div v-if="stat == false"
+  <!-- <div v-if="stat == false"
     class="z-50 fixed items-center flex justify-center flex-col inset-0 bg-black/50 backdrop-blur-sm">
     <div class="text-start bg-white rounded px-7 py-7 space-y-2">
       <p class="text-xl font-semibold">Duh Bot Nya Lagi <span class="bg-red-500 p-1 text-white rounded">Offline</span> </p>
       <p>Silahkan Contact Ke Nomor Berikut</p>
+      <ol class="text-start list-disc pl-4">
+        <li>0895-6037-92033 / Alif</li>
+        <li>0896-3695-1323 / Rafif</li>
+      </ol>
+    </div>
+  </div> -->
+  <div v-if="stat == false"
+    class="z-50 fixed items-center flex justify-center flex-col inset-0 bg-black/50 backdrop-blur-sm">
+    <div class="text-start bg-white rounded px-7 py-7 space-y-2">
+      <p class="text-xl font-semibold">Sedang Dalam Perbaikan Server<span class="bg-red-500 p-1 text-white rounded">Maintain</span> </p>
+      <p>Punya pertanyaan? Contact Kenomor berikut: </p>
       <ol class="text-start list-disc pl-4">
         <li>0895-6037-92033 / Alif</li>
         <li>0896-3695-1323 / Rafif</li>
